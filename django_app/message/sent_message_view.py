a2a362c24382e368ede90578f43b6a6d7a8879b7
ArtemKinko <artem.kinko.dev@gmail.com>
2024-02-26 18:37:59 +0300

    Добавлена инструкция, а также исправлены некоторые моменты

--- django_app/message/sent_message_view.py
+++ django_app/message/sent_message_view.py
@@ -5,9 +5,3 @@ from django.views.generic import TemplateView
 class MessageSentView(TemplateView):
     template_name = 'message_sent_view.html'
     success_url = reverse_lazy('message_view')
-
-    # def form_valid(self, form):
-    #     instance = form.save(commit=False)
-    #     print(instance)
-    #     print("Пришло сообщение: ", instance.text, ". Дата отправки:", instance.dt_upload)
-    #     return super().form_valid(form)
